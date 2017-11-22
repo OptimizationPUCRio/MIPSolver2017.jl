b05da90cd41d643f8f5fa03fc51964046fe23ebf
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-11-22 19:51:05 -0200

    Fixing some bugs

--- branch_and_bound.jl
+++ branch_and_bound.jl
@@ -47,10 +47,10 @@ function branch(currentNode::node, binaryIndices::Vector{Int64})
   return leftChild, rightChild
 end
 
-function obtainBoundList(nodeList::Vector{node})
-  boundList = Array{Float64}(length(nodes))
-  for i = 1 : length(nodes)
-    boundList[i] = nodes[i].model.objVal
+function obtainBoundList(nodeList)
+  boundList = Array{Float64}(length(nodeList))
+  for i = 1 : length(nodeList)
+    boundList[i] = nodeList[i].model.objVal
   end
 
   return boundList
@@ -77,34 +77,39 @@ function solveMIP(m::JuMP.Model)
   binarySolutions = 0
 
   # Solve linear relaxation
-  status = solve(m, relaxation=true)
+  m.colCat[:] = :Cont
+  status = solve(m)
+  nodes = Vector{node}(0)
   if status == :Optimal && isBinary(m, binaryIndices)
     # Solution of the relaxed problem is binary: optimal solution
-    nodes = Vector{node}(0)
     bestBound = m.objVal
     bestVal = m.objVal
     binarySolutions = 1
   else
-    # Create branch and bound tree
-    nodes = Vector{node}(1)
-    nodes[1] = node(0, m) # root
+    push!(nodes, node(0, m)) # Add root to branch and bound tree
     lastNodeLevel = 0
   end
 
-  iter = 0
-  flagOpt = 0
-  tol = 0.01 # Tolerance (%)
+  iter = 1 # number of visited nodes
+  flagOpt = 0 # flag that indicates if a viable solution has been found
+  branched = false # flag that indicates if a branch has occurred in this iteration
+  traverse = 1 # 1 for breadth, -1 for depth
+  tol = 1e-5 # tolerance (%)
   time0 = time_ns()
-  while !isempty(nodes) && abs((bestVal - bestBound)/bestVal) > tol && (time_ns()-time0)/1e9 < 600
-    if iter == 0
-      iter = 1
-      continue
+
+  while !isempty(nodes) && abs((bestVal - bestBound)/bestVal) > tol && (time_ns()-time0)/1e9 < 600000
+
+    # Change traverse method every 10 iterations for better bound discovery
+    if iter%10 == 0
+      traverse = (-1)*traverse
     end
+    branched = false
     # Check node lower bound. If greater than current best UB, prune by limit
     if nodes[1].model.objVal <= bestVal
       status = solve(nodes[1].model)
       if status == :Optimal
-        bestBound = minimum(obtainBoundList(nodes))
+        boundList = obtainBoundList(nodes)
+        bestBound = minimum(boundList)
         if isBinary(nodes[1].model, binaryIndices)
           # Relaxed solution is binary: optimal solution -- don't branch
           if nodes[1].model.objVal < bestVal
@@ -116,9 +121,17 @@ function solveMIP(m::JuMP.Model)
         elseif nodes[1].model.objVal <= bestVal
           # Relaxed solution is not binary and should not be pruned by limit -- branch
           (leftChild, rightChild) = branch(nodes[1], binaryIndices)
-          push!(nodes, leftChild)
-          push!(nodes, rightChild)
+          branched = true
         end
+      end
+    end
+
+    # Check if branched -- if true, add children to the list
+    if branched == true
+      if traverse == 1 # breadth -- insert children at the end of the list
+        push!(nodes, leftChild, rightChild)
+      else # depth -- insert children at the beginning of the list
+        unshift!(nodes, leftChild, rightChild)
       end
     end
     lastNodeLevel = nodes[1].level
@@ -139,6 +152,9 @@ function solveMIP(m::JuMP.Model)
     m.objVal = bestVal
     m.objBound = bestBound
   end
+
+  # Return binary variables to original state
+  m.colCat[binaryIndices] = :Bin
 
   # Outputs
   m.ext[:status] = status
